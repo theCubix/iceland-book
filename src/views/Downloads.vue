ebc7072c9e86bec23b1e339a18975b74d7a6af8e
[thecubix] <[timon.forrer@gmail.com]>
2018-07-21 16:45:23 +0200

    Added placeholder views

---
+++ src/views/Downloads.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to the {{ greeting }}-Page!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'downloads',
+  data () {
+    return {
+      greeting: 'Downloads'
+    }
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to the {{ greeting }}-Page!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      greeting: 'Login'
+    }
+  }
+}
+</script>
