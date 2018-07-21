dbdf006ea2852d2258df026acd69c92790b3dbb8
[thecubix] <[timon.forrer@gmail.com]>
2018-07-21 16:44:34 +0200

    Added firebase and markdown interpreter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueFire from 'vuefire'
 import './registerServiceWorker'
+import VueMarkdown from 'vue-markdown'
 
 Vue.config.productionTip = false
+
+Vue.use(VueFire)
 
 new Vue({
   router,
