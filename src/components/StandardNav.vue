8918e3cf9875c6f1e0fecdbaa7b06c5f6a4c2cab
[thecubix] <[timon.forrer@gmail.com]>
2018-07-21 17:53:38 +0200

    Added Nav Component

---
+++ src/components/StandardNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/journey">Journey</router-link>
+      <router-link to="/downloads">Downloads</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StandardNav'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
