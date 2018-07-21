451cc6eba119be0f532c8746abf6c208efe31f92
[thecubix] <[timon.forrer@gmail.com]>
2018-07-21 17:32:20 +0200

    Removed unused code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,17 @@
 <template>
   <div>
-    <h1>Welcome to the {{ greeting }}-Page!</h1>
-    <div v-for="(day, index) in journey" :key="index">
-      <img :src="day.cover" alt="">
-      <h2>{{day.title}}</h2>
-      <vue-markdown>{{day.text}}</vue-markdown>
-    </div>
   </div>
 </template>
 
 <script>
-import {db} from '@/firebase.js'
-import VueMarkdown from 'vue-markdown'
-
 export default {
   name: 'home',
   data () {
-    return {
-      greeting: 'Home',
-      journey: []
-    }
-  },
-  firestore () {
-    return {
-      journey: db.collection('journey').orderBy('day')
-    }
-  },
-  components: {
-    VueMarkdown
+
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
