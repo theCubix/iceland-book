13301e38f3c5ea5073aee94fdfa210d47a15ffe2
[thecubix] <[timon.forrer@gmail.com]>
2018-07-21 22:35:07 +0200

    Added global styles (view transition)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <component :is="layout">
-      <router-view/>
+      <transition name="fade">
+        <router-view/>
+      </transition>
     </component>
   </div>
 </template>
@@ -18,5 +20,24 @@ export default {
 
 
 <style lang="scss">
+@import './scss/variables';
 
+body {
+  background-color: $blue-light;
+  font-size: 16px;
+  margin: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
 </style>
