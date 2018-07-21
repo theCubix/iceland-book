fcacce5ca1abcbfafa50339fe0f9aec4a4b12bd9
[thecubix] <[timon.forrer@gmail.com]>
2018-07-21 17:32:41 +0200

    Not sure

--- src/views/Journey.vue
+++ src/views/Journey.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
     <h1>Welcome to the {{ greeting }}-Page!</h1>
+    <router-link
+      v-for="(day, index) in journey"
+      :key="index"
+      :to="'/journey/'+day.slug">
+        {{day.title}}<br/>
+    </router-link>
   </div>
 </template>
 
 <script>
+import {db} from '@/firebase.js'
+
 export default {
   name: 'journey',
   data () {
     return {
-      greeting: 'Journey'
+      greeting: 'Journey',
+      journey: []
+    }
+  },
+  firestore () {
+    return {
+      journey: db.collection('journey').orderBy('day')
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 320px;
+  max-width: 100%;
+}
+</style>
