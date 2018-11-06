7c3160550c9243898121f8cf99fec3973f653bcb
[thecubix] <[timon.forrer@gmail.com]>
2018-11-06 22:28:07 +0100

    Removed unused styles

--- src/views/Day.vue
+++ src/views/Day.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <day />
-    <page-control :previous="previous" :next="next" />
+    <page-control :active="active" />
   </div>
 </template>
 
 <script>
-import app from '@/firebase.js'
+// import app from '@/firebase.js'
 
 import PageControl from '@/components/PageControl.vue'
 import Day from '@/components/DayDetail.vue'
@@ -15,10 +15,7 @@ export default {
   name: 'Day',
   data () {
     return {
-      active: null,
-      entriesLength: null,
-      previous: null,
-      next: null
+      active: null
     }
   },
   components: {
@@ -28,42 +25,14 @@ export default {
   methods: {
     setActive () {
       this.active = Number(this.$route.params.day)
-    },
-    getAll () {
-      app.content.get('days', { fields: [ 'id' ] })
-        .then(res => {(
-          this.entriesLength = Object.keys(res).length)
-          this.setPrevious()
-          this.setNext()
-        })
-        .catch(err => console.log(err))
-    },
-    setPrevious () {
-      if (this.active > 1) {
-        let prev = this.active - 1
-        this.previous = prev
-      } else {
-        this.previous = null
-      }
-    },
-    setNext () {
-      if (this.active < this.entriesLength) {
-        let nex = this.active + 1
-        this.next = nex
-      } else {
-        this.next = null
-      }
     }
   },
   created () {
     this.setActive()
-    this.getAll()
   },
   watch: {
     $route (to, from) {
       this.setActive()
-      this.setPrevious()
-      this.setNext()
     }
   }
 }
