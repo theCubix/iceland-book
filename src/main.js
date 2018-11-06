437a829a09741813c359000772937067ef5e2667
[thecubix] <[timon.forrer@gmail.com]>
2018-11-06 22:26:48 +0100

    Removed unused package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import VueFire from 'vuefire'
 import VueMoment from 'vue-moment'
 import router from './router'
 import './registerServiceWorker'
@@ -13,7 +12,6 @@ Vue.component('not-logged-in-layout', NotLoggedIn)
 
 Vue.config.productionTip = false
 
-// Vue.use(VueFire)
 Vue.use(VueMoment)
 
 new Vue({
